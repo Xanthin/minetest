78511681bb5d24da3722975303da6cb99c56747c
Duane Robertson <duane@duanerobertson.com>
2015-12-21 21:57:26 -0600

    Change licensing by permission of Gael de Sailly. Update minetest.conf.example.

--- src/mapgen_valleys.cpp
+++ src/mapgen_valleys.cpp
@@ -6,20 +6,25 @@ Based on Valleys Mapgen by Gael de Sailly
  (https://forum.minetest.net/viewtopic.php?f=9&t=11430)
 and mapgen_v7 by kwolekr, Ryan Kwolek <kwolekr@minetest.net>.
 
+Licensing changed by permission of Gael de Sailly.
+
 This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3.0 of the License, or
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Lesser General Public License for more details.
 
-GNU GENERAL PUBLIC LICENSE
-Version 3, 29 June 2007
-
-See http://www.gnu.org/licenses/gpl-3.0.en.html
+You should have received a copy of the GNU Lesser General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3.0 of the License, or
+(at your option) any later version.
 */
 
 
